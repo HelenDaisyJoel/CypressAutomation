6f59ecf3fd187e412db853da07e887587f779299
HelenDaisyJoel <helendaisy.joel@gmail.com>
2024-09-27 13:34:36 +0530

    Added test

---
+++ cypress/e2e/MyFirstTest.cy.js
@@ -0,0 +1,14 @@
+describe('example to-do app', () => {
+    it('Test1',()=>{
+        cy.visit('https://tutorialsninja.com/demo/');
+        cy.title().should("eq","Your Store");
+    }
+    )
+    it('Test1',()=>{
+        cy.visit('https://tutorialsninja.com/demo/');
+        cy.title().should("eq","Your Store");
+    }
+    )
+      
+    })
+  
